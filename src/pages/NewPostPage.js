b1eeac116ce3e1f1f64c60eb8cee6f75f4d50e40
Max Campos <mcampos2a@gmail.com>
2025-07-02 13:01:46 +0200

    task: include required attribute for form control

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -29,19 +29,19 @@ const NewPostPage = () => {
             <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type='text' name='title' placeholder='Title' onChange={handleChange}/>
+                    <Form.Control type='text' name='title' placeholder='Title' onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Author</Form.Label>
-                    <Form.Control type='text' name='author' placeholder='Author' onChange={handleChange}/>
+                    <Form.Control type='text' name='author' placeholder='Author' onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Image URL</Form.Label>
-                    <Form.Control type='text' name='image' placeholder='Image URL' onChange={handleChange}/>
+                    <Form.Control type='text' name='image' placeholder='Image URL' onChange={handleChange} required/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Content</Form.Label>
-                    <Form.Control type='textarea' row={5} name='content' placeholder='Content' onChange={handleChange}/>
+                    <Form.Control type='textarea' row={5} name='content' placeholder='Content' onChange={handleChange} required/>
                 </Form.Group>
                 <Button variant='primary' type='submit'>
                     Create
